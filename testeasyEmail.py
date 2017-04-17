37ed2ea109e30c76e8224de9ac6e8ccffcb53ee7
zhuangchaoxi <zhuangchaoxi@lamahui.com>
2017-04-17 18:32:17 +0800

    Update testeasyEmail.py

--- testeasyEmail.py
+++ testeasyEmail.py
@@ -1,10 +1,10 @@
 #encoding: u8
-from easyEamil import easyEamil
+from easyEmail import easyEmail
 
 
 def sendtextmail(mail_host, mail_user, mail_pass, mailto_list):
     mail_title = "发送纯文件邮件,不支持内置图片"
-    mobj = easyEamil(mail_host, mail_user, mail_pass)
+    mobj = easyEmail(mail_host, mail_user, mail_pass)
     res = mobj.send_email_threds(mailto_list, 
                                  mail_title, 
                                  "text", 
@@ -14,7 +14,7 @@ def sendtextmail(mail_host, mail_user, mail_pass, mailto_list):
 
 def sendhtmlmail(mail_host, mail_user, mail_pass, mailto_list):
     mail_title = "发送html邮件,支持内置图片"
-    mobj = easyEamil(mail_host, mail_user, mail_pass)
+    mobj = easyEmail(mail_host, mail_user, mail_pass)
     res = mobj.send_email_threds(mailto_list, 
                                  mail_title, 
                                  "html", 
