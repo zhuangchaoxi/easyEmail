28acf26b921586926fed450210537d48132121ef
zhuangchaoxi <zhuangchaoxi@lamahui.com>
2017-04-17 18:24:54 +0800

    Update testeasyEmail.py

--- testeasyEmail.py
+++ testeasyEmail.py
@@ -3,22 +3,24 @@ from easyEamil import easyEamil
 
 
 def sendtextmail(mail_host, mail_user, mail_pass, mailto_list):
-    mail_title = "这是一个测试邮件"
+    mail_title = "发送纯文件邮件,不支持内置图片"
     mobj = easyEamil(mail_host, mail_user, mail_pass)
     res = mobj.send_email_threds(mailto_list, 
                                  mail_title, 
                                  "text", 
-                                 ["hello\nhaha"],
-                                 ["d:\\1.txt"])
+                                 ["hello\nhaha"], #文本
+                                 ["d:\\1.txt"] #附件
+                                 )
 
 def sendhtmlmail(mail_host, mail_user, mail_pass, mailto_list):
-    mail_title = "这是一个测试邮件"
+    mail_title = "发送html邮件,支持内置图片"
     mobj = easyEamil(mail_host, mail_user, mail_pass)
     res = mobj.send_email_threds(mailto_list, 
                                  mail_title, 
                                  "html", 
-                                 ["<b>详见下图:</b>","d:\\1.jpg", "d:\\2.jpg"], 
-                                 ["d:\\1.txt","d:\\2.txt"])
+                                 ["<b>详见下图:</b>","d:\\1.jpg", "d:\\2.jpg"], #html+内置图片
+                                 ["d:\\1.txt","d:\\2.txt"] #附件
+                                 )  
 
 def main():
     mail_host = "smtp.exmail.qq.com"
